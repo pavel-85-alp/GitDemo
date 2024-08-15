c512aec5238fd221ad58ddb2c01daf5978459217
pavel-85-alp <kolkata_pavel@yahoo.co.in>
2024-08-15 18:30:46 +0530

    Second commit

--- src/helloWorld/myTest1.java
+++ src/helloWorld/myTest1.java
@@ -18,10 +18,16 @@ public class myTest1 {
 		System.out.println("Hello Test 1");
 	}
 	
+	@Test
+	public void test2()
+	{
+		System.out.println("Hello Test 2");
+	}
+	
 	@AfterTest
 	public void test3()
 	{
-		System.out.println("Hello Test 2");
+		System.out.println("Hello Test 3");
 	}
 	
 }
