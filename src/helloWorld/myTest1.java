701a699cfa46679afd182592ad803ca43329a566
pavel-85-alp <kolkata_pavel@yahoo.co.in>
2024-08-15 18:20:14 +0530

    first commit

---
+++ src/helloWorld/myTest1.java
@@ -0,0 +1,27 @@
+package helloWorld;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class myTest1 {
+
+	@BeforeTest
+	public void test0()
+	{
+		System.out.println("Hello Test 0");
+	}
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Hello Test 1");
+	}
+	
+	@AfterTest
+	public void test3()
+	{
+		System.out.println("Hello Test 2");
+	}
+	
+}
